38ccb6b64903d0cbccc5916e18bf2869d2813746
adarsh-s-nayak <pes1ug20cs620@pesu.pes.edu>
2022-10-09 00:40:33 +0530

    updated

--- tracks/App.js
+++ tracks/App.js
@@ -1,3 +1,8 @@
+import { createAppContainer,createSwitchNavigator} from "react-navigation";
+import { createStackNavigator } from "react-navigation-stack";
+import { createBottomTabNavigator } from "react-navigation-tabs";
+
+//importing all the files in here 
 import AccountScreen from "./src/screens/AccountScreen";
 import SigninScreen from "./src/screens/SigninScreen";
 import SignupScreen from "./src/screens/SignupScreen";
@@ -5,3 +10,23 @@ import TrackCreateScreen from "./src/screens/TrackCreateScreen";
 import TrackDetailScreen from "./src/screens/TrackDetailScreen";
 import TrackListScreen from "./src/screens/TrackListScreen";
 
+//we have switch navigator , bottom navigator and stack navigator.
+// we need to put them from top down.
+
+const switchNavigator = createSwitchNavigator({
+    loginFlow: createStackNavigator({
+      Signup:SignupScreen,
+      Signin:SigninScreen
+    }),
+    mainFlow: createBottomTabNavigator({
+      trackListFlow : createStackNavigator({
+          TrackList:TrackListScreen,
+          TrackDetail:TrackDetailScreen,
+      }),
+      TrackCreate : TrackCreateScreen,
+      Account : AccountScreen,
+    })
+
+});
+
+export default createAppContainer(switchNavigator);
--- tracks/src/screens/TrackListScreen.js
+++ tracks/src/screens/TrackListScreen.js
@@ -1,9 +1,15 @@
 import React from "react";
-import {View,StyleSheet,Text} from 'react-native';
+import {View,StyleSheet,Text,Button} from 'react-native';
 
 
-const TrackListScreen = () => {
-    return <Text style={{fontSize:48}}>TrackListScreen</Text>
+const TrackListScreen = ({navigation}) => {
+    return <>
+        <Text style={{fontSize:48}}>TrackListScreen</Text>
+        <Button  
+            title="go to track detail"
+            onPress={()=>navigation.navigate('TrackDetail')}
+        />
+    </> 
 }
 
 const styles = StyleSheet.create({
